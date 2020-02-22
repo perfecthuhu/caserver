f7ddc430bc040db504096f291a3667dd4dbd1561
liumingyu <liumingyu04@meituan.com>
2020-02-22 19:30:17 +0800

    修复

--- ca-core/src/main/java/com/card/alumni/entity/CaAlumniAuditLog.java
+++ ca-core/src/main/java/com/card/alumni/entity/CaAlumniAuditLog.java
@@ -1,14 +1,7 @@
 package com.card.alumni.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Date;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CaAlumniAuditLog {
     private Integer id;
 
@@ -24,4 +17,73 @@ public class CaAlumniAuditLog {
 
     private Date updateTime;
 
+    public CaAlumniAuditLog(Integer id, Integer studentId, Integer alumniId, Integer auditStatus, Integer auidtorId, Date createTime, Date updateTime) {
+        this.id = id;
+        this.studentId = studentId;
+        this.alumniId = alumniId;
+        this.auditStatus = auditStatus;
+        this.auidtorId = auidtorId;
+        this.createTime = createTime;
+        this.updateTime = updateTime;
+    }
+
+    public CaAlumniAuditLog() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+
+    public Integer getAlumniId() {
+        return alumniId;
+    }
+
+    public void setAlumniId(Integer alumniId) {
+        this.alumniId = alumniId;
+    }
+
+    public Integer getAuditStatus() {
+        return auditStatus;
+    }
+
+    public void setAuditStatus(Integer auditStatus) {
+        this.auditStatus = auditStatus;
+    }
+
+    public Integer getAuidtorId() {
+        return auidtorId;
+    }
+
+    public void setAuidtorId(Integer auidtorId) {
+        this.auidtorId = auidtorId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 }
