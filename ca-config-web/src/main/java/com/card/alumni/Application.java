faa2249c2e5815254b789d53b318db0bf54461e9
liuyang <liuyyang.liu@qunar.com>
2020-05-03 19:07:17 +0800

    线上环境部署 打包日志优化

--- ca-config-web/src/main/java/com/card/alumni/Application.java
+++ ca-config-web/src/main/java/com/card/alumni/Application.java
@@ -3,7 +3,9 @@ package com.card.alumni;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author liumingyu
@@ -12,10 +14,15 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @Log4j2
 @SpringBootApplication
 @ServletComponentScan
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("ca-config-web was started...");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
